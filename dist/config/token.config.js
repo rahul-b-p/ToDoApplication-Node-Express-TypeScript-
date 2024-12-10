7e3db882ad65b7e11ab88c7488a97d17002a603b
rahul-b-p <rahulzantoz10@gmail.com>
2024-12-10 11:50:06 +0530

    removed promice rduntancy on token config

--- dist/config/token.config.js
+++ dist/config/token.config.js
@@ -15,29 +15,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkTokenBlacklist = exports.blackListToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const redis_util_1 = __importDefault(require("../utils/redis.util"));
+const winston_util_1 = require("../utils/winston.util");
 const blackListToken = (token) => __awaiter(void 0, void 0, void 0, function* () {
-    return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
-        try {
-            const { exp } = jsonwebtoken_1.default.decode(token);
-            const expiresIn = exp - Math.floor(Date.now() / 1000);
-            const result = yield redis_util_1.default.set(token, 'Blacklisted', { 'EX': expiresIn });
-            resolve(result);
-        }
-        catch (error) {
-            reject(error);
-        }
-    }));
+    try {
+        const { exp } = jsonwebtoken_1.default.decode(token);
+        const expiresIn = exp - Math.floor(Date.now() / 1000);
+        const result = yield redis_util_1.default.set(token, 'Blacklisted', { 'EX': expiresIn });
+        return result;
+    }
+    catch (error) {
+        winston_util_1.loggers.error(error);
+        throw new Error("Can't Blacklist Token");
+    }
 });
 exports.blackListToken = blackListToken;
 const checkTokenBlacklist = (token) => __awaiter(void 0, void 0, void 0, function* () {
-    return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
-        try {
-            const result = yield redis_util_1.default.get(token);
-            resolve(result);
-        }
-        catch (error) {
-            reject(error);
-        }
-    }));
+    try {
+        const result = yield redis_util_1.default.get(token);
+        return result;
+    }
+    catch (error) {
+        winston_util_1.loggers.error(error);
+        throw new Error("Can't check the token now");
+    }
 });
 exports.checkTokenBlacklist = checkTokenBlacklist;
--- src/config/token.config.ts
+++ src/config/token.config.ts
@@ -4,26 +4,24 @@ import redisClient from '../utils/redis.util';
 import { loggers } from '../utils/winston.util';
 
 
-export const blackListToken = async(token: string) => {
-    return new Promise(async(resolve, reject) => {
-        try {
-            const {exp} = jwt.decode(token) as JwtPayload;
-            const expiresIn = exp - Math.floor(Date.now()/1000);
-            const result = await redisClient.set(token, 'Blacklisted',{'EX':expiresIn});
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    });
+export const blackListToken = async (token: string): Promise<string | null> => {
+    try {
+        const { exp } = jwt.decode(token) as JwtPayload;
+        const expiresIn = exp - Math.floor(Date.now() / 1000);
+        const result = await redisClient.set(token, 'Blacklisted', { 'EX': expiresIn });
+        return result;
+    } catch (error) {
+        loggers.error(error);
+        throw new Error("Can't Blacklist Token");
+    }
 }
 
-export const checkTokenBlacklist=async(token:string)=>{
-    return new Promise(async(resolve,reject)=>{
-        try {
-            const result = await redisClient.get(token);
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    })
+export const checkTokenBlacklist = async (token: string):Promise<string|null> => {
+    try {
+        const result = await redisClient.get(token);
+        return result;
+    } catch (error) {
+        loggers.error(error);
+       throw new Error("Can't check the token now");
+    }
 }
